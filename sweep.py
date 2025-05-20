b34c805e585ea25675b27a4c6cdd09eb5b338c80
venkatesh <venkateshvenky1982002@gmail.com>
2025-05-20 13:11:49 +0530

    fix: added sweep params for with attention and without attention

--- sweep.py
+++ sweep.py
@@ -3,7 +3,8 @@ from train import train
 
 sweep_config = {
     'method': 'bayes',
-    'name': 'translit-seq2seq-attention',
+    # 'name': 'translit-seq2seq-attention',
+    'name': 'translit-seq2seq-tamil',
     'metric': {
         'name': 'val_accuracy',
         'goal': 'maximize'
@@ -11,8 +12,10 @@ sweep_config = {
     'parameters': {
         'emb_dim': {'values': [16,32,64,256]},
         'hid_dim': {'values': [16,32,64,256]},
-        'enc_layers': {'values': [1,2]},
-        'dec_layers': {'values': [1,2]},
+        'enc_layers': {'values': [1,2,3]},
+        # 'enc_layers': {'values': [1]},
+        'dec_layers': {'values': [1,2,3]},
+        'dec_layers': {'values': [1]},
         'cell_type': {'values': ['rnn','gru','lstm']},
         'dropout': {'values': [0.0,0.1]},
         'lr': {'values': [1e-2,1e-4]},
@@ -20,12 +23,14 @@ sweep_config = {
         'beam_size':   {'values': [1,5,10]},
         'max_len': { 'values': [32]},
         'epochs': {'values': [15,20]},
-        'use_attention': {'values': [True]},
+        'use_attention': {'values': [False]},
+        # 'use_attention': {'values': [True]},
         'bidirectional': {'values': [True, False]},
+        # 'bidirectional': {'values': [False]},
         'teacher_forcing_ratio':{'values': [0.0,0.2]},
     }
 }
 
 if __name__ == '__main__':
-    sweep_id = wandb.sweep(sweep_config, project='DA6401_Assignment3')
+    sweep_id = wandb.sweep(sweep_config, project='test-project')
     wandb.agent(sweep_id, function=lambda: train()) #count=100
